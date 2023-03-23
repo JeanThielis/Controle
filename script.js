84609a27c44ca4a9b098f3a594a31d1477b94218
Jean Thielis <jeanthielis@gmail.com>
2023-03-23 12:37:47 -0300

    versao_03

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
             url: menu,
             success: function (response) {
                 $("#resultado").html(response);
+                console.log(menu);
 
             }
         });
