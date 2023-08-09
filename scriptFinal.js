a107b4a35dd2f0249bbb11614c6840f8b2f76730
Jean Thielis <jeanthielis@gmail.com>
2023-08-09 14:57:24 -0300

    Versao 09/08/23

--- scriptFinal.js
+++ scriptFinal.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
   vetorRelatorio =[];
   function limparDados(){
-    $("#referencia,#lote,#linha,#situacao,#relatorio").val(" ");
+    $("#referencia,#lote,#linha,#situacao,#observacao").val(" ");
    }
    function alertConfirm(tipo,titulo,mensagem,time){
     Swal.fire({
@@ -23,7 +23,7 @@ $(document).ready(function(){
     var lote = $ ("#lote").val();
     var linha = $ ("#linha").val();
     var situacao = $("#situacao").val();
-    var relatorio = $("#relatorio").val();
+    observacao = $("#observacao").val();
 
 
     cabecalhoFixo = "\n*Relatório Final*" +
@@ -31,37 +31,40 @@ $(document).ready(function(){
         "\n*Equipe:* " + equipe +
         "\n*Data:* " + data_nova;
 
-    relatorio ="\n*Linha:* " + linha +
+    relatorio ="\n\n*Linha:* " + linha +
         "\n*Referência:* " + referencia +
         "\n*Lote:* " + lote +
         "\n*Situação:* "+ situacao+
-        "\n*Observações:* \n"+relatorio+
+        "\n*Observações:* \n"+observacao+
         "\n";
 
-
-    vetorRelatorio.push(relatorio);
-    resultadoRelatorioNovo = vetorRelatorio.join('____________________________________________');
-    $("#resultadoFinal").val(cabecalhoFixo+resultadoRelatorioNovo);
-    alertConfirm('success','Legal','Dados Inserido com Sucesso',3000)
-    limparDados();
-  });
-
-  $("#enviarRelatorio").click(function(){
-    var resultadoFinal=$("#resultadoFinal").val();
-    if(resultadoFinal==""){
-      alertConfirm('info','Antenção','Você não inserio os dados',3000);
+    if(observacao!=""){
+      vetorRelatorio.push(relatorio);
+      resultadoRelatorioNovo = vetorRelatorio.join('_____________________________________');
+      $("#textResultadoFinal").val(cabecalhoFixo+resultadoRelatorioNovo);
+      $("#visualizar-final").fadeIn("slow");
+      alertConfirm('success','Legal','Dados Inserido com Sucesso',3000)
+      limparDados();
 
     }else{
+      alertConfirm('error','Erro','Compo de obersavação vazio',3000);
+      $("#observacao").focus();
+    }
+    
+  });
+
+  $("#enviarFinal").click(function(){
       try {
-        resultado=$("#resultadoFinal").val();
+        resultado=$("#textResultadoFinal").val();
         conteudo = encodeURIComponent(resultado);
-        document.getElementById('compartilharFinal').href="https://api.whatsapp.com/send?text="+conteudo;
+        document.getElementById('compartilhar').href="https://api.whatsapp.com/send?text="+conteudo;
+        $("#textResultadoFinal").val(" ");
     
         } catch (error) {
         alertConfirm('error','Erro','Houve  problema no envio',3000);
     
         }
-    }
+    
           
   
 })
